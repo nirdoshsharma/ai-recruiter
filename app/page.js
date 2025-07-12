6d578f1f43658a9807edda9424a861bf277150bc
Nirdosh <nirdosh@Nirdoshs-MacBook-Air.local>
2025-07-12 20:04:27 +0530

    chore(login)

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { supabase } from "./services/supabaseClient";
 
 export default function Home() {
   const router = useRouter();
@@ -11,9 +12,20 @@ export default function Home() {
     router.push("/dashboard"); // adjust the path based on your routing
   };
 
+  const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4">
       <h1 className="text-3xl font-bold">Ai - Recruiter</h1>
+      <Button onClick={signInWithGoogle}>Login With Google</Button>
       <Button onClick={handleNavigate}>Go to Dashboard</Button>
     </div>
   );
